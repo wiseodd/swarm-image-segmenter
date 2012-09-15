12422b7729447035ca831f7d01fe6c66480947ca
Agustinus Kristiadi <kristiadi7@gmail.com>
2012-09-15 12:54:36 +0700

    Fix indentation

--- src/pso_cluster.h
+++ src/pso_cluster.h
@@ -33,38 +33,38 @@ const int DATA_DIM = 3;
 
 struct data
 {
-	int info[DATA_DIM];
+    int info[DATA_DIM];
 };
 
 struct particle
 {
-	data *position;
-	data *pBest;
-	data *velocity;
+    data *position;
+    data *pBest;
+    data *velocity;
 };
 
 struct GBest
 {
-	short *gBestAssign;
-	data *centroids;
-	int *arrCentroids;
+    short *gBestAssign;
+    data *centroids;
+    int *arrCentroids;
 };
 
 float getRandom(float low, float high);
 float getRandomClamped();
 float getDistance(data first, data second);
 float fitness(const short *assignMat, const data *datas, const data *centroids, 
-			  int data_size, int cluster_size);
+              int data_size, int cluster_size);
 void assignDataToCentroid(short *assignMat, const data *datas, 
-						  const data *centroids, int data_size, 
-						  int cluster_size);
+                          const data *centroids, int data_size, 
+                          int cluster_size);
 void initializePSO(particle *particles, GBest& gBest, const data *datas, 
-				   int data_size, int particle_size, int cluster_size);
+                   int data_size, int particle_size, int cluster_size);
 GBest hostPsoClustering(data *datas, int data_size, int particle_size, 
-						int cluster_size, int max_iter);
+                        int cluster_size, int max_iter);
 extern "C" float devFitness(short* assignMat, int* datas, int* centroids, 
-							int data_size, int cluster_size);
+                            int data_size, int cluster_size);
 extern "C" GBest devicePsoClustering(data *datas, int *flatDatas, int data_size, 
-									 int particle_size, int cluster_size, 
-									 int max_iter);
+                                     int particle_size, int cluster_size, 
+                                     int max_iter);
 #endif /* PSO_CLUSTER_H */
