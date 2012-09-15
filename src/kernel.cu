36eff262dd7b71dea0cff900fb4c3e29f9231a9a
Agustinus Kristiadi <kristiadi7@gmail.com>
2012-09-15 12:49:10 +0700

    Fix indentation

--- src/kernel.cu
+++ src/kernel.cu
@@ -7,15 +7,15 @@
 __host__ __device__ 
 float devGetDistance(int *first, int *second)
 {
-	float total = 0.0f;
+    float total = 0.0f;
 
-	for (int i = 0; i < DATA_DIM; i++)
-	{
-		int res = (first[i] - second[i]);
-		total += res * res;
-	}
+    for (int i = 0; i < DATA_DIM; i++)
+    {
+        int res = (first[i] - second[i]);
+        total += res * res;
+    }
 
-	return sqrt(total);
+    return sqrt(total);
 }
 
 /*
@@ -23,25 +23,25 @@ float devGetDistance(int *first, int *second)
  */
 __host__ __device__ 
 float devFitness(short* assignMat, int* datas, int* centroids, int data_size, 
-				 int cluster_size)
+                 int cluster_size)
 {
-	float total = 0.0f;
+    float total = 0.0f;
 
-	for (int i = 0; i < cluster_size; i++)
-	{
-		float subtotal = 0.0f;
+    for (int i = 0; i < cluster_size; i++)
+    {
+        float subtotal = 0.0f;
 
-		for (int j = 0; j < data_size; j++)
-		{
-			if (assignMat[j] == i)
-				subtotal += devGetDistance(&datas[j * DATA_DIM], 
-										   &centroids[i * DATA_DIM]);
-		}
+        for (int j = 0; j < data_size; j++)
+        {
+            if (assignMat[j] == i)
+                subtotal += devGetDistance(&datas[j * DATA_DIM], 
+                                           &centroids[i * DATA_DIM]);
+        }
 
-		total += subtotal / data_size;
-	}
+        total += subtotal / data_size;
+    }
 
-	return total / cluster_size;
+    return total / cluster_size;
 }
 
 /*
@@ -49,260 +49,260 @@ float devFitness(short* assignMat, int* datas, int* centroids, int data_size,
  */
 __host__ __device__ 
 void devAssignDataToCentroid(short *assignMat, int *datas, int *centroids, 
-							 int data_size, int cluster_size)
+                             int data_size, int cluster_size)
 {
-	for (int i = 0; i < data_size; i++)
-	{
-		int nearestCentroidIdx = 0;
-		float nearestCentroidDist = INF;
+    for (int i = 0; i < data_size; i++)
+    {
+        int nearestCentroidIdx = 0;
+        float nearestCentroidDist = INF;
 
-		for (int j = 0; j < cluster_size; j++)
-		{
-			float nearestDist = devGetDistance(&datas[i * DATA_DIM], 
-											   &centroids[j * DATA_DIM]);
+        for (int j = 0; j < cluster_size; j++)
+        {
+            float nearestDist = devGetDistance(&datas[i * DATA_DIM], 
+                                               &centroids[j * DATA_DIM]);
 
-			if (nearestDist < nearestCentroidDist)
-			{
-				nearestCentroidDist = nearestDist;
-				nearestCentroidIdx = j;
-			}
-		}
+            if (nearestDist < nearestCentroidDist)
+            {
+                nearestCentroidDist = nearestDist;
+                nearestCentroidIdx = j;
+            }
+        }
 
-		assignMat[i] = nearestCentroidIdx;
-	}
+        assignMat[i] = nearestCentroidIdx;
+    }
 }
 
 /*
  * Initialize necessary variables for PSO
  */
 void initialize(int *positions, int *velocities, int *pBests, int *gBest, 
-				const data* datas, int data_size, int particle_size, 
-				int cluster_size)
+                const data* datas, int data_size, int particle_size, 
+                int cluster_size)
 {
-	for (int i = 0; i < particle_size * cluster_size * DATA_DIM; i+= DATA_DIM)
-	{
-		int rand = round(getRandom(0, data_size - 1));
+    for (int i = 0; i < particle_size * cluster_size * DATA_DIM; i+= DATA_DIM)
+    {
+        int rand = round(getRandom(0, data_size - 1));
 
-		for(int j = 0; j < DATA_DIM; j++)
-		{
-			positions[i + j] = datas[rand].info[j];
-			pBests[i + j] = datas[rand].info[j];
-			velocities[i + j] = 0;
-		}
-	}
+        for(int j = 0; j < DATA_DIM; j++)
+        {
+            positions[i + j] = datas[rand].info[j];
+            pBests[i + j] = datas[rand].info[j];
+            velocities[i + j] = 0;
+        }
+    }
 
-	for(int i = 0; i < cluster_size * DATA_DIM; i++)
-		gBest[i] = pBests[i];
+    for(int i = 0; i < cluster_size * DATA_DIM; i++)
+        gBest[i] = pBests[i];
 }
 
 /*
  * Kernel to update particle
  */
 __global__ void kernelUpdateParticle(int *positions, int *velocities, 
-									 int *pBests, int *gBest, short *posAssign, 
-									 int* datas, float rp, float rg, 
-									 int data_size, int particle_size, 
-									 int cluster_size)
+                                     int *pBests, int *gBest, short *posAssign, 
+                                     int* datas, float rp, float rg, 
+                                     int data_size, int particle_size, 
+                                     int cluster_size)
 {
-	int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-	if(i >= particle_size * cluster_size * DATA_DIM)
-		return;
+    if(i >= particle_size * cluster_size * DATA_DIM)
+        return;
 
-	// Update particle velocity and position
-	velocities[i] = (int)lroundf(OMEGA * velocities[i] 
-					+ c1 * rp * (pBests[i] - positions[i])
-					+ c2 * rg * (gBest[i % (cluster_size * DATA_DIM)] 
-					- positions[i]));
+    // Update particle velocity and position
+    velocities[i] = (int)lroundf(OMEGA * velocities[i] 
+                    + c1 * rp * (pBests[i] - positions[i])
+                    + c2 * rg * (gBest[i % (cluster_size * DATA_DIM)] 
+                    - positions[i]));
 
-	positions[i] += velocities[i];
+    positions[i] += velocities[i];
 }
 
 /*
  * Kernel to update particle
  */
 __global__ void kernelUpdatePBest(int *positions, int *pBests, short *posAssign, 
-								  short *pBestAssign, float *fitnesses, 
-								  int* datas, int data_size, int particle_size, 
-								  int cluster_size)
+                                  short *pBestAssign, float *fitnesses, 
+                                  int* datas, int data_size, int particle_size, 
+                                  int cluster_size)
 {
-	int i = blockIdx.x * blockDim.x + threadIdx.x;
-	int offsetParticle = i * cluster_size * DATA_DIM;
-	int offsetAssign = i * data_size;
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int offsetParticle = i * cluster_size * DATA_DIM;
+    int offsetAssign = i * data_size;
 
-	if(i >= particle_size)
-		return;
+    if(i >= particle_size)
+        return;
 
-	devAssignDataToCentroid(&posAssign[offsetAssign], datas, 
-							&positions[offsetParticle], data_size, cluster_size);
+    devAssignDataToCentroid(&posAssign[offsetAssign], datas, 
+                            &positions[offsetParticle], data_size, cluster_size);
 
-	fitnesses[i] = devFitness(&pBestAssign[offsetAssign], datas, 
-							  &pBests[offsetParticle], data_size, cluster_size);
+    fitnesses[i] = devFitness(&pBestAssign[offsetAssign], datas, 
+                              &pBests[offsetParticle], data_size, cluster_size);
 
-	// Update pBest
-	if (devFitness(&posAssign[offsetAssign], datas, &positions[offsetParticle], 
-				   data_size, cluster_size)
-		< fitnesses[i])
-	{
-		// Update pBest position
-		for (int k = 0; k < cluster_size * DATA_DIM; k++)
-			pBests[offsetParticle + k] = positions[offsetParticle + k];
+    // Update pBest
+    if (devFitness(&posAssign[offsetAssign], datas, &positions[offsetParticle], 
+                   data_size, cluster_size)
+        < fitnesses[i])
+    {
+        // Update pBest position
+        for (int k = 0; k < cluster_size * DATA_DIM; k++)
+            pBests[offsetParticle + k] = positions[offsetParticle + k];
 
-		// Update pBest assignment matrix
-		for(int k = 0; k < data_size; k++)
-			pBestAssign[offsetAssign + k] = posAssign[offsetAssign + k];
-	}
+        // Update pBest assignment matrix
+        for(int k = 0; k < data_size; k++)
+            pBestAssign[offsetAssign + k] = posAssign[offsetAssign + k];
+    }
 }
 
 __global__ void kernelUpdateGBest(int *gBest, int *pBests, int offset, 
-								  int cluster_size)
+                                  int cluster_size)
 {
-	int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-	if(i >= cluster_size * DATA_DIM)
-		return;
+    if(i >= cluster_size * DATA_DIM)
+        return;
 
-	gBest[i] = pBests[offset + i];
+    gBest[i] = pBests[offset + i];
 }
 
 __global__ void kernelUpdateGBestAssign(short *gBestAssign, short *pBestAssign, 
-										int offset, int data_size)
+                                        int offset, int data_size)
 {
-	int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-	if(i >= data_size)
-		return;
+    if(i >= data_size)
+        return;
 
-	gBestAssign[i] = pBestAssign[offset + i];
+    gBestAssign[i] = pBestAssign[offset + i];
 }
 
 /*
  * Wrapper to initialize and running PSO on device
  */
 extern "C" GBest devicePsoClustering(data *datas, int *flatDatas, int data_size, 
-									 int particle_size, int cluster_size, 
-									 int max_iter)
+                                     int particle_size, int cluster_size, 
+                                     int max_iter)
 {
-	// Initialize host memory
-	int *positions = new int[particle_size * cluster_size * DATA_DIM];
-	int *velocities = new int[particle_size * cluster_size * DATA_DIM];
-	int *pBests = new int[particle_size * cluster_size * DATA_DIM];
-	int *gBest = new int[cluster_size * DATA_DIM];
-	short *posAssign = new short[particle_size * data_size];
-	short *pBestAssign = new short[particle_size * data_size];
-	short *gBestAssign = new short[data_size];
+    // Initialize host memory
+    int *positions = new int[particle_size * cluster_size * DATA_DIM];
+    int *velocities = new int[particle_size * cluster_size * DATA_DIM];
+    int *pBests = new int[particle_size * cluster_size * DATA_DIM];
+    int *gBest = new int[cluster_size * DATA_DIM];
+    short *posAssign = new short[particle_size * data_size];
+    short *pBestAssign = new short[particle_size * data_size];
+    short *gBestAssign = new short[data_size];
 
-	// Initialize assignment matrix to cluster 0
-	for(int i = 0; i < particle_size * data_size; i++)
-	{
-		posAssign[i] = 0;
-		pBestAssign[i] = 0;
+    // Initialize assignment matrix to cluster 0
+    for(int i = 0; i < particle_size * data_size; i++)
+    {
+        posAssign[i] = 0;
+        pBestAssign[i] = 0;
 
-		if(i < data_size)
-			gBestAssign[i] = 0;
-	}
+        if(i < data_size)
+            gBestAssign[i] = 0;
+    }
 
-	initialize(positions, velocities, pBests, gBest, datas, data_size, 
-			   particle_size, cluster_size);
+    initialize(positions, velocities, pBests, gBest, datas, data_size, 
+               particle_size, cluster_size);
 
-	// Initialize device memory
-	int *devPositions, *devVelocities, *devPBests, *devGBest;
-	short *devPosAssign, *devPBestAssign, *devGBestAssign;
-	int *devDatas;
-	float *devFitnesses;
+    // Initialize device memory
+    int *devPositions, *devVelocities, *devPBests, *devGBest;
+    short *devPosAssign, *devPBestAssign, *devGBestAssign;
+    int *devDatas;
+    float *devFitnesses;
 
-	size_t size = sizeof(int) * particle_size * cluster_size * DATA_DIM;
-	size_t assign_size = sizeof(short) * particle_size * data_size;
+    size_t size = sizeof(int) * particle_size * cluster_size * DATA_DIM;
+    size_t assign_size = sizeof(short) * particle_size * data_size;
 
-	cudaMalloc((void**)&devPositions, size);
-	cudaMalloc((void**)&devVelocities, size);
-	cudaMalloc((void**)&devPBests, size);
-	cudaMalloc((void**)&devGBest, sizeof(int) * cluster_size * DATA_DIM);
-	cudaMalloc((void**)&devPosAssign, assign_size);
-	cudaMalloc((void**)&devPBestAssign, assign_size);
-	cudaMalloc((void**)&devGBestAssign, sizeof(short) * data_size);	
-	cudaMalloc((void**)&devDatas, sizeof(int) * data_size * DATA_DIM);
-	cudaMalloc((void**)&devFitnesses, sizeof(float) * particle_size);
+    cudaMalloc((void**)&devPositions, size);
+    cudaMalloc((void**)&devVelocities, size);
+    cudaMalloc((void**)&devPBests, size);
+    cudaMalloc((void**)&devGBest, sizeof(int) * cluster_size * DATA_DIM);
+    cudaMalloc((void**)&devPosAssign, assign_size);
+    cudaMalloc((void**)&devPBestAssign, assign_size);
+    cudaMalloc((void**)&devGBestAssign, sizeof(short) * data_size); 
+    cudaMalloc((void**)&devDatas, sizeof(int) * data_size * DATA_DIM);
+    cudaMalloc((void**)&devFitnesses, sizeof(float) * particle_size);
 
-	// Copy data from host to device
-	cudaMemcpy(devPositions, positions, size, cudaMemcpyHostToDevice);
-	cudaMemcpy(devVelocities, velocities, size, cudaMemcpyHostToDevice);
-	cudaMemcpy(devPBests, pBests, size, cudaMemcpyHostToDevice);
-	cudaMemcpy(devGBest, gBest, sizeof(int) * cluster_size * DATA_DIM, 
-			   cudaMemcpyHostToDevice);
-	cudaMemcpy(devPosAssign, posAssign, assign_size, cudaMemcpyHostToDevice);
-	cudaMemcpy(devPBestAssign, pBestAssign, assign_size, cudaMemcpyHostToDevice);	
-	cudaMemcpy(devGBestAssign, gBestAssign, sizeof(short) * data_size, 
-			   cudaMemcpyHostToDevice);
-	cudaMemcpy(devDatas, flatDatas, sizeof(int) * data_size * DATA_DIM, 
-			   cudaMemcpyHostToDevice);
+    // Copy data from host to device
+    cudaMemcpy(devPositions, positions, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(devVelocities, velocities, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(devPBests, pBests, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(devGBest, gBest, sizeof(int) * cluster_size * DATA_DIM, 
+               cudaMemcpyHostToDevice);
+    cudaMemcpy(devPosAssign, posAssign, assign_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(devPBestAssign, pBestAssign, assign_size, cudaMemcpyHostToDevice);   
+    cudaMemcpy(devGBestAssign, gBestAssign, sizeof(short) * data_size, 
+               cudaMemcpyHostToDevice);
+    cudaMemcpy(devDatas, flatDatas, sizeof(int) * data_size * DATA_DIM, 
+               cudaMemcpyHostToDevice);
 
-	// Wrap device pointer to device pBests array
-	thrust::device_ptr<float> dptrFitness(devFitnesses);
-	thrust::device_ptr<float> resPtr;
+    // Wrap device pointer to device pBests array
+    thrust::device_ptr<float> dptrFitness(devFitnesses);
+    thrust::device_ptr<float> resPtr;
 
-	// Threads and blocks number
-	int threads = 32;
-	int blocksPart = (particle_size / threads) + 1;
-	int blocksFull = (particle_size * cluster_size * DATA_DIM / threads) + 1;
+    // Threads and blocks number
+    int threads = 32;
+    int blocksPart = (particle_size / threads) + 1;
+    int blocksFull = (particle_size * cluster_size * DATA_DIM / threads) + 1;
 
-	// Iteration
-	for (int iter = 0; iter < max_iter; iter++)
-	{
-		float rp = getRandomClamped();
-		float rg = getRandomClamped();
+    // Iteration
+    for (int iter = 0; iter < max_iter; iter++)
+    {
+        float rp = getRandomClamped();
+        float rg = getRandomClamped();
 
-		kernelUpdateParticle<<<blocksFull, threads>>>
-			(devPositions, devVelocities, devPBests, devGBest, devPosAssign, 
-			 devDatas, rp, rg, data_size, particle_size, cluster_size);	    
+        kernelUpdateParticle<<<blocksFull, threads>>>
+            (devPositions, devVelocities, devPBests, devGBest, devPosAssign, 
+             devDatas, rp, rg, data_size, particle_size, cluster_size);     
 
-		kernelUpdatePBest<<<blocksPart, threads>>>
-			(devPositions, devPBests, devPosAssign, devPBestAssign, 
-			 devFitnesses, devDatas, data_size, particle_size, cluster_size);
+        kernelUpdatePBest<<<blocksPart, threads>>>
+            (devPositions, devPBests, devPosAssign, devPBestAssign, 
+             devFitnesses, devDatas, data_size, particle_size, cluster_size);
 
-		// Get min element
-		resPtr = thrust::min_element(dptrFitness, dptrFitness + particle_size);
-		// Cast to raw pointer
-		int index = resPtr - dptrFitness;
+        // Get min element
+        resPtr = thrust::min_element(dptrFitness, dptrFitness + particle_size);
+        // Cast to raw pointer
+        int index = resPtr - dptrFitness;
 
-		int offsetParticle = index * cluster_size * DATA_DIM;
-		int offsetAssign = index * data_size;
+        int offsetParticle = index * cluster_size * DATA_DIM;
+        int offsetAssign = index * data_size;
 
-		// // Update gBest on device
-		kernelUpdateGBest<<<((cluster_size * DATA_DIM) / threads) + 1, threads>>>
-			(devGBest, devPBests, offsetParticle, cluster_size);
+        // // Update gBest on device
+        kernelUpdateGBest<<<((cluster_size * DATA_DIM) / threads) + 1, threads>>>
+            (devGBest, devPBests, offsetParticle, cluster_size);
 
-		// Update gBest assignment matrix on device
-		kernelUpdateGBestAssign<<<(data_size / threads) + 1, threads>>>
-			(devGBestAssign, devPBestAssign, offsetAssign, data_size);
-	}
+        // Update gBest assignment matrix on device
+        kernelUpdateGBestAssign<<<(data_size / threads) + 1, threads>>>
+            (devGBestAssign, devPBestAssign, offsetAssign, data_size);
+    }
 
-	// Copy gBest from device to host
-	cudaMemcpy(gBest, devGBest, sizeof(int) * cluster_size * DATA_DIM, 
-			   cudaMemcpyDeviceToHost);
-	cudaMemcpy(gBestAssign, devGBestAssign, sizeof(short) * data_size, 
-			   cudaMemcpyDeviceToHost);
+    // Copy gBest from device to host
+    cudaMemcpy(gBest, devGBest, sizeof(int) * cluster_size * DATA_DIM, 
+               cudaMemcpyDeviceToHost);
+    cudaMemcpy(gBestAssign, devGBestAssign, sizeof(short) * data_size, 
+               cudaMemcpyDeviceToHost);
 
-	// Cleanup
-	delete[] positions;
-	delete[] velocities;
-	delete[] pBests;
-	delete[] posAssign;
-	delete[] pBestAssign;
+    // Cleanup
+    delete[] positions;
+    delete[] velocities;
+    delete[] pBests;
+    delete[] posAssign;
+    delete[] pBestAssign;
 
-	cudaFree(devPositions);
-	cudaFree(devVelocities);
-	cudaFree(devPBests);
-	cudaFree(devGBest);
-	cudaFree(devPosAssign);
-	cudaFree(devPBestAssign);
-	cudaFree(devGBestAssign);
-	cudaFree(devDatas);
-	cudaFree(devFitnesses);
+    cudaFree(devPositions);
+    cudaFree(devVelocities);
+    cudaFree(devPBests);
+    cudaFree(devGBest);
+    cudaFree(devPosAssign);
+    cudaFree(devPBestAssign);
+    cudaFree(devGBestAssign);
+    cudaFree(devDatas);
+    cudaFree(devFitnesses);
 
-	GBest gBestReturn;
-	gBestReturn.gBestAssign = gBestAssign;
-	gBestReturn.arrCentroids = gBest;
+    GBest gBestReturn;
+    gBestReturn.gBestAssign = gBestAssign;
+    gBestReturn.arrCentroids = gBest;
 
-	return gBestReturn;
+    return gBestReturn;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -10,146 +10,146 @@
 
 int main(int argc, char** argv)
 {
-	srand(time(NULL));
-	IplImage* inputImage = NULL;
-	inputImage = cvLoadImage("/home/adi/Pictures/DSC00156.resized.resized.jpg", 
-							 -1);
+    srand(time(NULL));
+    IplImage* inputImage = NULL;
+    inputImage = cvLoadImage("/home/adi/Pictures/DSC00156.resized.resized.jpg", 
+                             -1);
 
-	int width = inputImage->width;
-	int height = inputImage->height;
-	int channel = inputImage->nChannels;
+    int width = inputImage->width;
+    int height = inputImage->height;
+    int channel = inputImage->nChannels;
 
-	char *arrImage = new char[width * height * channel];
-	int *flatDatas = new int[width * height * channel];
-	data *datas = new data[width * height];
+    char *arrImage = new char[width * height * channel];
+    int *flatDatas = new int[width * height * channel];
+    data *datas = new data[width * height];
 
-	// Load image to array
-	for (int i = 0; i < width * height; i++)
-	{
-		arrImage[i * channel + 0] = 
-			(unsigned char) inputImage->imageData[i * channel + 0];
-		arrImage[i * channel + 1] = 
-			(unsigned char) inputImage->imageData[i * channel + 1];
-		arrImage[i * channel + 2] = 
-			(unsigned char) inputImage->imageData[i * channel + 2];
+    // Load image to array
+    for (int i = 0; i < width * height; i++)
+    {
+        arrImage[i * channel + 0] = 
+            (unsigned char) inputImage->imageData[i * channel + 0];
+        arrImage[i * channel + 1] = 
+            (unsigned char) inputImage->imageData[i * channel + 1];
+        arrImage[i * channel + 2] = 
+            (unsigned char) inputImage->imageData[i * channel + 2];
 
-		flatDatas[i * channel + 0] = 
-			(unsigned char) inputImage->imageData[i * channel + 0];
-		flatDatas[i * channel + 1] = 
-			(unsigned char) inputImage->imageData[i * channel + 1];
-		flatDatas[i * channel + 2] = 
-			(unsigned char) inputImage->imageData[i * channel + 2];
+        flatDatas[i * channel + 0] = 
+            (unsigned char) inputImage->imageData[i * channel + 0];
+        flatDatas[i * channel + 1] = 
+            (unsigned char) inputImage->imageData[i * channel + 1];
+        flatDatas[i * channel + 2] = 
+            (unsigned char) inputImage->imageData[i * channel + 2];
 
-		data d;
+        data d;
 
-		d.info[0] = (unsigned char) inputImage->imageData[i * channel + 0];
-		d.info[1] = (unsigned char) inputImage->imageData[i * channel + 1];
-		d.info[2] = (unsigned char) inputImage->imageData[i * channel + 2];
+        d.info[0] = (unsigned char) inputImage->imageData[i * channel + 0];
+        d.info[1] = (unsigned char) inputImage->imageData[i * channel + 1];
+        d.info[2] = (unsigned char) inputImage->imageData[i * channel + 2];
 
-		datas[i] = d;
-	}
+        datas[i] = d;
+    }
 
-	// PSO parameters
-	int particle_num, cluster_num, max_iter;
-	char comp;
+    // PSO parameters
+    int particle_num, cluster_num, max_iter;
+    char comp;
 
-	cout << "Number of cluster : ";
-	cin >> cluster_num;
-	cout << "Number of particle : ";
-	cin >> particle_num;
-	cout << "Number of iteration : ";
-	cin >> max_iter;
+    cout << "Number of cluster : ";
+    cin >> cluster_num;
+    cout << "Number of particle : ";
+    cin >> particle_num;
+    cout << "Number of iteration : ";
+    cin >> max_iter;
 
-	do
-	{
-		cout << "CPU or GPU (C / G) ? : ";
-		cin >> comp;
-	}
-	while(comp != 'C' && comp != 'G');
+    do
+    {
+        cout << "CPU or GPU (C / G) ? : ";
+        cin >> comp;
+    }
+    while(comp != 'C' && comp != 'G');
 
-	cout << endl;
+    cout << endl;
 
-	clock_t begin = clock();
+    clock_t begin = clock();
 
-	GBest gBest;
+    GBest gBest;
 
-	// Check if use host code or device code
-	if(comp == 'C')
-		gBest = hostPsoClustering(datas, width * height, particle_num, 
-								  cluster_num, max_iter);
-	else
-		gBest = devicePsoClustering(datas, flatDatas, width * height, 
-									particle_num, cluster_num, max_iter);
+    // Check if use host code or device code
+    if(comp == 'C')
+        gBest = hostPsoClustering(datas, width * height, particle_num, 
+                                  cluster_num, max_iter);
+    else
+        gBest = devicePsoClustering(datas, flatDatas, width * height, 
+                                    particle_num, cluster_num, max_iter);
 
-	clock_t end = clock();
+    clock_t end = clock();
 
-	cout << "Time elapsed : " << (double)(end - begin) / CLOCKS_PER_SEC << "s" 
-		 << endl;
+    cout << "Time elapsed : " << (double)(end - begin) / CLOCKS_PER_SEC << "s" 
+         << endl;
 
-	// Compute quantization error of clusters, less is better
-	if(comp == 'C')
-	{
-		cout << "Quantization Error : " 
-			 << fitness(gBest.gBestAssign, datas, gBest.centroids, 
-			 			width * height, cluster_num) 
-			 << endl;
-	}
-	else
-	{
-		cout << "Quantization Error : " 
-			 << devFitness(gBest.gBestAssign, flatDatas, gBest.arrCentroids, 
-			 	width * height, cluster_num) 
-			 << endl;		
-	}
+    // Compute quantization error of clusters, less is better
+    if(comp == 'C')
+    {
+        cout << "Quantization Error : " 
+             << fitness(gBest.gBestAssign, datas, gBest.centroids, 
+                        width * height, cluster_num) 
+             << endl;
+    }
+    else
+    {
+        cout << "Quantization Error : " 
+             << devFitness(gBest.gBestAssign, flatDatas, gBest.arrCentroids, 
+                width * height, cluster_num) 
+             << endl;       
+    }
 
-	// List for cluster color
-	unsigned char colorList[9][3] =	{ { 0, 0, 255 }, { 255, 0, 0 }, 
-									  { 0, 255, 0 }, { 255, 255, 0 }, 
-									  { 255, 0, 255 }, { 255, 128, 128 }, 
-									  { 128, 128, 128 }, { 128, 0, 0 }, 
-									  { 255, 128, 0 } };
+    // List for cluster color
+    unsigned char colorList[9][3] = { { 0, 0, 255 }, { 255, 0, 0 }, 
+                                      { 0, 255, 0 }, { 255, 255, 0 }, 
+                                      { 255, 0, 255 }, { 255, 128, 128 }, 
+                                      { 128, 128, 128 }, { 128, 0, 0 }, 
+                                      { 255, 128, 0 } };
 
-	// Coloring clusters
-	for (int i = 0; i < width * height; i++)
-	{
-		for (int j = 0; j < cluster_num; j++)
-		{
-			if (gBest.gBestAssign[i] == j)
-			{
-				arrImage[i * channel + 0] = colorList[j][0];
-				arrImage[i * channel + 1] = colorList[j][1];
-				arrImage[i * channel + 2] = colorList[j][2];
-			}
-		}
-	}
+    // Coloring clusters
+    for (int i = 0; i < width * height; i++)
+    {
+        for (int j = 0; j < cluster_num; j++)
+        {
+            if (gBest.gBestAssign[i] == j)
+            {
+                arrImage[i * channel + 0] = colorList[j][0];
+                arrImage[i * channel + 1] = colorList[j][1];
+                arrImage[i * channel + 2] = colorList[j][2];
+            }
+        }
+    }
 
-	cudaFree(start);
-	cudaFree(stop);
+    cudaFree(start);
+    cudaFree(stop);
 
-	if(comp == 'C')
-		delete[] gBest.centroids;
-	else
-		delete[] gBest.arrCentroids;
+    if(comp == 'C')
+        delete[] gBest.centroids;
+    else
+        delete[] gBest.arrCentroids;
 
-	delete[] gBest.gBestAssign;
+    delete[] gBest.gBestAssign;
 
-	// Write array to image
-	IplImage* outImage = cvCreateImage(cvSize(width, height), 
-									   inputImage->depth, channel);
-	outImage->imageData = arrImage;
+    // Write array to image
+    IplImage* outImage = cvCreateImage(cvSize(width, height), 
+                                       inputImage->depth, channel);
+    outImage->imageData = arrImage;
 
-	cvSaveImage("out1.jpg", outImage);
+    cvSaveImage("out1.jpg", outImage);
 
-	cvNamedWindow("Result");
-	cvShowImage("Result", outImage);
-	cvWaitKey(0);
+    cvNamedWindow("Result");
+    cvShowImage("Result", outImage);
+    cvWaitKey(0);
 
-	// Cleanup
-	cvReleaseImage(&inputImage);
-	cvReleaseImage(&outImage);
+    // Cleanup
+    cvReleaseImage(&inputImage);
+    cvReleaseImage(&outImage);
 
-	delete[] arrImage;
+    delete[] arrImage;
 
-	return 0;
+    return 0;
 }
 
